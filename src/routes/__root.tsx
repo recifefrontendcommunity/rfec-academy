0ad3c2238e58022a15fe666e8720ebbe5ad61b68
Alisson Santos <alissonlima529@gmail.com>
2025-12-25 22:35:01 -0300

    chore: update gtag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -94,11 +94,17 @@ export const Route = createRootRoute({
     scripts: GTM_ID
       ? [
           {
-            children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
+            async: true,
+            src: `https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`,
+          },
+          {
+            children: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${GTM_ID}');
+            `,
             type: "text/javascript",
           },
         ]
@@ -115,16 +121,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        {GTM_ID && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-        )}
         {children}
         <TanStackDevtools
           config={{
